6404e0bcb096e4b6d6e220c89f8d98cdc8b8ae18
tt1991 <tt1991518@hotmail.com>
2017-07-06 13:55:40 +0200

    chore(app): back to normal

--- server/processes/web/controllers/health-check/actions/health-check.spec.js
+++ server/processes/web/controllers/health-check/actions/health-check.spec.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const request = require('supertest');
-const app = require('../../../index');
+const createApp = require('../../../index');
 
 describe('Health check', function() {
   it('should respond with success', async function() {
-    const response = await request(app.listen())
+    const response = await request(createApp().listen())
       .get('/healthcheck')
       .expect('Content-Type', /json/)
       .expect(200);
